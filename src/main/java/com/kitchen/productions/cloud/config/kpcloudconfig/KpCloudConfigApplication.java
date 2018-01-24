cfaa2f188f542dea1cdb0306255faa714d00fb25
Taylor Dumas <taylorjdumas@gmail.com>
2018-01-23 20:01:17 -0600

    Initial Commit

---
+++ src/main/java/com/kitchen/productions/cloud/config/kpcloudconfig/KpCloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.kitchen.productions.cloud.config.kpcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpCloudConfigApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kitchen/productions/cloud/config/kpcloudconfig/KpCloudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kitchen.productions.cloud.config.kpcloudconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KpCloudConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
