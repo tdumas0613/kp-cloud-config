073122930cf8fb03422ab7cbc5a4161e70259aa4
Taylor Dumas <taylorjdumas@gmail.com>
2018-02-03 14:31:31 -0600

    adjust application.properties

--- src/main/java/com/kitchen/productions/cloud/config/kpcloudconfig/KpCloudConfigApplication.java
+++ src/main/java/com/kitchen/productions/cloud/config/kpcloudconfig/KpCloudConfigApplication.java
@@ -2,7 +2,9 @@ package com.kitchen.productions.cloud.config.kpcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class KpCloudConfigApplication {
 
